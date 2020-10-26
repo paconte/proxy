8c249e37369daa8c83caf95d9b14d4fee58f2a5b
Francisco Revilla <paconte@gmail.com>
2020-10-26 17:25:31 +0100

    Add better debugging to the standart output.

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -27,12 +27,24 @@ def _get_jwt_header():
 
 def print_request(req):
     """methods for debugging purposes"""
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+    body = ""
+    try:
+        body = req.body
+    except AttributeError:
+        pass
+    print('{}\n{}\n{}\n{}\n'.format(
         '-----------START-----------',
         req.method + ' ' + req.url,
         '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        'body: ' + str(body),
         '-----------END-------------'
     ))
+
+
+def debug_request(method, url, json_data, headers):
+    req = requests.Request(
+        method, url, json=json_data, headers=headers).prepare()
+    print_request(req)
 
 
 class MyProxy(BaseHTTPRequestHandler):
@@ -57,15 +69,33 @@ class MyProxy(BaseHTTPRequestHandler):
     def _get_endpoint(self):
         return proxy_endpoint + self.path
 
+    def _load_json(self):
+        json_data = None
+        value = self.headers.get('Content-Type', False)
+        if 'json' in value:
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            json_data = json.loads(post_data.decode('utf-8'))
+        return json_data
+
+    def _endpoints_headers(self, json_data):
+        result = {}
+        jwt = _get_jwt_header()
+        result.update(jwt)
+        if json_data is not None:
+            result['Content-Type'] = 'application/json'
+            result['Content-Length'] = self.headers['Content-Length']
+        return result
+
     def do_GET(self):
         """HTTP GET action"""
         if proxy_status_path == self.path:
             self._set_get_response()
             post_requests = status.Status.requests()
             running_time = status.Status.running_str()
-            self.wfile.write("<h1>Running since: {}</h1><br/>' \
-                '<h1>Requests: {}</h1>".format(
-                running_time, post_requests).encode('utf-8'))
+            self.wfile.write(
+                "<h1>Running since: {}</h1><br/><h1>Requests: {}</h1>".format(
+                    running_time, post_requests).encode('utf-8'))
         else:
             self.send_error(
                 404,
@@ -74,16 +104,11 @@ class MyProxy(BaseHTTPRequestHandler):
 
     def do_POST(self):
         """HTTP POST action"""
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        json_data = json.loads(post_data.decode('utf-8'))
-        jwt = _get_jwt_header()
-        request = requests.post(
-            url=self._get_endpoint(),
-            json=json_data,
-            headers=jwt)
-        req = requests.Request('POST', self._get_endpoint(), headers=jwt)
-        print_request(req)
+        json_data = self._load_json()
+        headers = self._endpoints_headers(json_data)
+        url = self._get_endpoint()
+        request = requests.post(url=url, json=json_data, headers=headers)
+        debug_request('POST', url, json_data, headers)
         status.Status.increment()
         self._set_post_response(request)
         self.wfile.write(request.content)
